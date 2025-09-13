916b518d6636b59951b66562eca499f9b6b9acbf
Laura Nielson <len2004@byu.edu>
2025-09-12 21:00:34 -0600

    implemting phase 0, implemented all pieceMoves except pawns.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -56,44 +56,108 @@ public class ChessPiece {
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece = board.getPiece(myPosition);
+        ChessGame.TeamColor myColor = pieceColor;
 
         List<ChessMove> moves = new ArrayList<>();
 
         if (piece.getPieceType() == PieceType.BISHOP){
-
             // Add all possible diagonals to move list
-            MoveInDirection(moves, myPosition, 1, 1);
-            MoveInDirection(moves, myPosition, -1,1);
-            MoveInDirection(moves, myPosition, -1, -1);
-            MoveInDirection(moves, myPosition, 1,-1);
+            MoveInDirection(moves, myPosition, 1, 1, board, myColor); // up-right
+            MoveInDirection(moves, myPosition, -1,1, board, myColor); // down-right
+            MoveInDirection(moves, myPosition, -1, -1, board, myColor); // down-left
+            MoveInDirection(moves, myPosition, 1,-1, board, myColor); // up-left
+        }
+        if (piece.getPieceType() == PieceType.ROOK){
+            MoveInDirection(moves, myPosition,1, 0, board, myColor); // up
+            MoveInDirection(moves, myPosition, -1, 0, board, myColor); // down
+            MoveInDirection(moves, myPosition, 0, 1, board, myColor); // right
+            MoveInDirection(moves, myPosition, 0, -1, board, myColor); // left
+        }
+        if (piece.getPieceType() == PieceType.KNIGHT){
+            MoveSinglePos(moves, myPosition, 2, 1, board, myColor);
+            MoveSinglePos(moves, myPosition, -2, 1, board, myColor);
+            MoveSinglePos(moves, myPosition, 2, -1, board, myColor);
+            MoveSinglePos(moves, myPosition, -2, -1, board, myColor);
+            MoveSinglePos(moves, myPosition, 1, 2, board, myColor);
+            MoveSinglePos(moves, myPosition, -1, 2, board, myColor);
+            MoveSinglePos(moves, myPosition, 1, -2, board, myColor);
+            MoveSinglePos(moves, myPosition, -1, -2, board, myColor);
+        }
+        if (piece.getPieceType() == PieceType.KING) {
+            MoveSinglePos(moves, myPosition, 1, -1, board, myColor);
+            MoveSinglePos(moves, myPosition, 1, 0, board, myColor);
+            MoveSinglePos(moves, myPosition, 1, 1, board, myColor);
+            MoveSinglePos(moves, myPosition, 0, -1, board, myColor);
+            MoveSinglePos(moves, myPosition, 0,1, board, myColor);
+            MoveSinglePos(moves, myPosition, -1, -1, board, myColor);
+            MoveSinglePos(moves, myPosition, -1, 0, board, myColor);
+            MoveSinglePos(moves, myPosition, -1, 1, board, myColor);
+        }
+        if (piece.getPieceType() == PieceType.QUEEN) {
+            // Diagonals
+            MoveInDirection(moves, myPosition, 1, 1, board, myColor); // up-right
+            MoveInDirection(moves, myPosition, -1,1, board, myColor); // down-right
+            MoveInDirection(moves, myPosition, -1, -1, board, myColor); // down-left
+            MoveInDirection(moves, myPosition, 1,-1, board, myColor); // up-left
+            // Straights
+            MoveInDirection(moves, myPosition,1, 0, board, myColor); // up
+            MoveInDirection(moves, myPosition, -1, 0, board, myColor); // down
+            MoveInDirection(moves, myPosition, 0, 1, board, myColor); // right
+            MoveInDirection(moves, myPosition, 0, -1, board, myColor); // left
+        }
+        if (piece.getPieceType() == PieceType.PAWN){
+            //cry.
         }
         return moves;
     }
     // Check if position is valid on the board
     public boolean checkValidPosition(ChessPosition currentPosition){
         // Check if move on board.
-        //if (currentPosition.getRow() <= 8 &&
-        //currentPosition.getColumn() <= 8 &&
-        //        currentPosition.getRow() >= 1 &&
-         //       currentPosition.getColumn() >= 1){
-        //}
         return currentPosition.getRow() <= 8 &&
                 currentPosition.getColumn() <= 8 &&
                 currentPosition.getRow() >= 1 &&
                 currentPosition.getColumn() >= 1;
     }
     //move in a direction.
-    private void MoveInDirection(List<ChessMove> moves, ChessPosition startPos, int rowDir, int colDir){
+    private void MoveInDirection(List<ChessMove> moves, ChessPosition startPos, int rowDir, int colDir, ChessBoard board, ChessGame.TeamColor myColor){
         int row = startPos.getRow() +rowDir;
         int col = startPos.getColumn() + colDir;
 
+        //valid position & not blocked.
         while (checkValidPosition(new ChessPosition(row,col))){
             ChessPosition testPosition = new ChessPosition(row, col);
-            moves.add(new ChessMove(startPos, testPosition, null));
+            ChessPiece pieceAt = board.getPiece((testPosition));
 
-            row += rowDir;
-            col += colDir;
+            // Check if route is blocked.
+            if (pieceAt == null) { // no piece blocking route
+                moves.add(new ChessMove(startPos, testPosition, null));
+            } else { // if piece is opposite color add pos else break
+                if (checkCanCapture(pieceAt, board, myColor)) {
+                    moves.add(new ChessMove(startPos, testPosition, null));
+                }
+                break;
+            }
+
+        row += rowDir;
+        col += colDir;
         }
+    }
+    private void MoveSinglePos (List<ChessMove> moves, ChessPosition startPos, int rowDir, int colDir, ChessBoard board, ChessGame.TeamColor myColor){
+        ChessPosition testPosition = new ChessPosition(startPos.getRow()+rowDir, startPos.getColumn()+colDir);
+
+        if (checkValidPosition(testPosition)){
+            ChessPiece pieceAt = board.getPiece(testPosition);
+            if (pieceAt == null) {
+                moves.add(new ChessMove(startPos, testPosition, null));
+            } else {
+                if(checkCanCapture(pieceAt, board, myColor)){
+                    moves.add(new ChessMove(startPos, testPosition, null));
+                }
+            }
+        }
+    }
+    private boolean checkCanCapture(ChessPiece testPiece, ChessBoard board, ChessGame.TeamColor myColor){
+        return testPiece.getTeamColor() != myColor; // returns true if can capture, false if same team.
     }
 
     @Override
