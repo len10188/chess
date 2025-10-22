bbf57c73f6f64704868363f4cb423cd6d474a14e
Laura Nielson <len2004@byu.edu>
2025-10-21 21:02:40 -0600

    Finished implementing List Games.

---
+++ server/src/main/java/handler/ListGamesHandler.java
@@ -0,0 +1,49 @@
+package handler;
+
+import com.google.gson.Gson;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import request.ListGamesRequest;
+import result.ListGamesResult;
+import service.ListGamesService;
+import service.ServiceException;
+
+import java.util.Map;
+
+public class ListGamesHandler {
+    private ListGamesService listGamesService;
+    private final Gson gson = new Gson();
+
+    public ListGamesHandler(ListGamesService listGamesService){
+        this.listGamesService = listGamesService;
+    }
+
+    // Get /game (list all games)
+    public Handler listGames = ctx -> {
+        try {
+            // Get authToken from header
+            String authToken = ctx.header("Authorization");
+            ListGamesRequest request = new ListGamesRequest(authToken);
+
+            // Call service
+            ListGamesResult result = listGamesService.listGames(request);
+
+            // respond successful
+            ctx.status(200);
+            ctx.contentType("application/json");
+            ctx.result(gson.toJson(result));
+
+        } catch (ServiceException.UnauthorizedException e) {
+            sendError(ctx,401, "Error: unauthorized");
+        } catch (Exception e){
+            sendError(ctx, 500, "Error: " + e.getMessage());
+        }
+    };
+
+    private void sendError(Context ctx, int statusCode, String message){
+        var errorBody = Map.of("message", message);
+        ctx.status(statusCode);
+        ctx.contentType("application/json");
+        ctx.result(gson.toJson(errorBody));
+    }
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,17 +1,18 @@
 package server;
 
+
+import dataaccess.*;
+import handler.UserHandler;
 import handler.LoginHandler;
 import handler.LogoutHandler;
-import io.javalin.*;
-import handler.UserHandler;
-import org.eclipse.jetty.server.Authentication;
-import service.LoginService;
+import handler.ListGamesHandler;
+
 import service.UserService;
+import service.LoginService;
 import service.LogoutService;
-import dataaccess.MemoryAuthDAO;
-import dataaccess.MemoryUserDOA;
-import dataaccess.UserDAO;
-import dataaccess.AuthDAO;
+import service.ListGamesService;
+
+import io.javalin.*;
 
 public class Server {
 
@@ -25,22 +26,26 @@ public class Server {
         // Create DAO
         UserDAO userDAO = new MemoryUserDOA();
         AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
 
         // Create services
         UserService userService = new UserService(userDAO, authDAO);
         LoginService loginService = new LoginService(userDAO, authDAO);
         LogoutService logoutService = new LogoutService(authDAO);
+        ListGamesService listGamesService = new ListGamesService(authDAO, gameDAO);
+
 
         // Create handlers
         UserHandler userHandler = new UserHandler(userService);
         LoginHandler loginHandler = new LoginHandler(loginService);
         LogoutHandler logoutHandler = new LogoutHandler(logoutService);
+        ListGamesHandler listGamesHandler = new ListGamesHandler(listGamesService);
 
         // routes
         javalin.post("/user", userHandler.registerUser);
         javalin.post("/session", loginHandler.loginUser);
         javalin.delete("/session", logoutHandler.logoutUser);
-
+        javalin.get("/games", listGamesHandler.listGames);
 
     }
 
