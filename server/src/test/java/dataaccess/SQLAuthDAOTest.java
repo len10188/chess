3a6f13747325814fc52b4ea583618c81cb368c72
Laura Nielson <len2004@byu.edu>
2025-10-29 13:03:13 -0600

    start writing test for AuthDAO.

--- server/src/test/java/dataaccess/SQLAuthDAOTest.java
+++ server/src/test/java/dataaccess/SQLAuthDAOTest.java
@@ -1,12 +1,16 @@
 package dataaccess;
 
 import model.AuthData;
-import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 public class SQLAuthDAOTest {
+    private SQLAuthDAO authDAO;
 
-    private SQLAuthDAO
-
+    @BeforeEach
+    void setup() throws DataAccessException {
+        DAOSwitch.useDatabase();
+        authDAO = new SQLAuthDAO();
+        authDAO.clear();
+    }
 }
