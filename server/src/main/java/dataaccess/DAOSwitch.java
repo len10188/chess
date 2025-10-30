dd19c8198ed557994bb44f9df53beddbe2645602
Laura Nielson <len2004@byu.edu>
2025-10-29 20:30:22 -0600

    code quality.

--- server/src/main/java/dataaccess/DAOSwitch.java
+++ server/src/main/java/dataaccess/DAOSwitch.java
@@ -1,28 +1,28 @@
 package dataaccess;
 
 public class DAOSwitch {
-    private static boolean use_datebase = true;// false = memory, true = database.
+    private static boolean useDatebase = true;// false = memory, true = database.
 
     public static void useMemory() {
-        use_datebase = false;}
+        useDatebase = false;}
     public static void useDatabase() {
-        use_datebase = true;}
+        useDatebase = true;}
     public static GameDAO setGameDAO() throws DataAccessException {
-        if (use_datebase) {
+        if (useDatebase) {
             return new SQLGameDAO();
         } else {
             return new MemoryGameDAO();
         }
     }
     public static AuthDAO setAuthDAO() throws DataAccessException {
-        if (use_datebase) {
+        if (useDatebase) {
             return new SQLAuthDAO();
         } else {
             return new MemoryAuthDAO();
         }
     }
     public static UserDAO setUserDAO() throws DataAccessException {
-        if (use_datebase) {
+        if (useDatebase) {
             return new SQLUserDAO();
         } else {
             return new MemoryUserDOA();
