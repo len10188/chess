5a3a98b969229770458ec4f614cb53095b70ddea
Laura Nielson <len2004@byu.edu>
2025-11-11 20:50:59 -0700

    Start drawBoard.

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -1,4 +1,10 @@
 package ui;
 
+import chess.ChessGame;
+
 public class DrawBoard {
+    private DrawBoard() {}
+
+    private static final String LIGHT = EscapeSequences.SET_BG_COLOR_LIGHT_GREY;
+    priva
 }
