48338ed02538b6a8906aecc9a6a9896115a0d58b
Laura Nielson <len2004@byu.edu>
2025-12-01 14:01:04 -0700

    code quality add helper functions

--- client/src/main/java/ui/ReadEvalLoop.java
+++ client/src/main/java/ui/ReadEvalLoop.java
@@ -67,7 +67,7 @@ public class ReadEvalLoop implements MessageHandler {
         System.out.println(postLogin.welcome());
         System.out.println(postLogin.help());
 
-        while (state == UiState.LOGGED_IN || state ==UiState.IN_GAME) {
+        while (state == UiState.LOGGED_IN || state == UiState.IN_GAME) {
             printPrompt();
             String line = readLine(scanner);
             if (line == null) {
@@ -76,132 +76,15 @@ public class ReadEvalLoop implements MessageHandler {
 
             try {
                 if (state == UiState.LOGGED_IN) {
-                    // --------LOBBY MODE ---------
-                    var parts = line.trim().split("\\s+");
-                    if (parts.length == 0 || parts[0].isBlank()) {
-                        continue;
-                    }
-                    var cmd = parts[0].toLowerCase();
-
-                    // ---- JOIN ----
-                    if((cmd.equals("join") || cmd.equals("j"))) {
-                        if (parts.length != 3) {
-                            System.out.println("Usage: join <gameNumber> <white|black>");
-                            continue;
-                        }
-
-                        int gameNum = parseInt(parts[1]);
-                        String color = parts[2].toLowerCase();
-
-                        List<GameData> games = postLogin.getLastListedGames();
-                        if (gameNum < 1 || gameNum > games.size()) {
-                            System.out.println("Invalid game number. Use 'list' to see games.");
-                            continue;
-                        }
-
-                        if (!color.equals("white") && !color.equals("black")) {
-                            System.out.println("Color must be 'white' or 'black'.");
-                            continue;
-                        }
-
-                        GameData game = games.get(gameNum - 1);
-
-                        boolean joined;
-                        try {
-                            joined = postLogin.getFacade().joinGame(color, game.gameID());
-                        } catch (Exception ex) {
-                            System.out.println("Join failed: " + ex.getMessage());
-                            continue;
-                        }
-                        if (!joined) {
-                            continue;
-                        }
-
-                        ChessGame.TeamColor perspective;
-                        if ("white".equalsIgnoreCase(color)) {
-                            perspective = ChessGame.TeamColor.WHITE;
-                        } else {
-                            perspective = ChessGame.TeamColor.BLACK;
-                        }
-
-                        try {
-                            inGame = new InGameClient(serverUrl, authToken, game.gameID(), perspective);
-                            state = UiState.IN_GAME;
-                            System.out.println("Joined game as " + color + ".");
-                            System.out.println(inGame.welcome());
-                            System.out.println(inGame.help());
-                        } catch (Exception e) {
-                            System.out.println("Failed to enter game: " + e.getMessage());
-                        }
-                        continue;
-                    }
-
-                    // ------ WATCH ------
-                    if ((cmd.equals("watch") || cmd.equals("w"))) {
-                        if (parts.length != 2) {
-                            System.out.println("Usage: watch <gameNumber>");
-                            continue;
-                        }
-
-                        int gameNum = parseInt(parts[1]);
-                        List<GameData> games = postLogin.getLastListedGames();
-                        if (gameNum < 1 || gameNum > games.size()) {
-                            System.out.println("Invalid game number. Use 'list' to see games.");
-                            continue;
-                        }
-
-                        GameData game = games.get(gameNum - 1);
-
-                        try {
-                            inGame  = new InGameClient(serverUrl,
-                                    authToken,
-                                    game.gameID(),
-                                    ChessGame.TeamColor.WHITE);
-                            state = UiState.IN_GAME;
-                            System.out.println("Observing game.");
-                            System.out.println(inGame.help());
-                        } catch (Exception e) {
-                            System.out.println("Failed to observer game: " + e.getMessage());
-                        }
-                        continue;
-                    }
-
-                    String out = postLogin.eval(line);
-                    if (out == null || out.isBlank()) {
-                        continue;
-                    }
-
-                    if ("goodbye".equals(out)) {
-                        System.out.println("Logged out.");
-                        state = UiState.LOGGED_OUT;
-                        break;
-                    }
-                    // Postlogin needs to render board.
-                    if (out.startsWith("BOARD")) {
-                        System.out.println(out.substring("BOARD\n".length()));
-                        continue;
-                    }
-
-                    // everything else
-                    System.out.println(out);
+                    handleLoggedInLine(line, postLogin, authToken);
                 } else if (state == UiState.IN_GAME) {
-                    String out = inGame.eval(line);
-                    if (out != null && !out.isBlank()) {
-                        System.out.println(out);
-                    }
-
-                    if (out != null && out.startsWith("Leaving game.")) {
-                        state = UiState.LOGGED_IN;
-                        inGame = null;
-                        System.out.println(postLogin.help());
-                    }
+                    handleInGameLine(line, postLogin);
                 }
-            }catch(Throwable t){
+            } catch (Throwable t) {
                 System.out.println("Error: " + t.getClass().getSimpleName());
             }
         }
     }
-
     private void printPrompt() {
         String tag;
         switch(state){
@@ -230,4 +113,74 @@ public class ReadEvalLoop implements MessageHandler {
             return -1;
         }
     }
+
+    private void handleLoggedInLine(String line, PostLoginClient postLogin, String authToken) {
+        var parts = line.trim().split("\\s+");
+        if (parts.length == 0 || parts[0].isBlank()) {
+            return;
+        }
+        var cmd = parts[0].toLowerCase();
+
+        // JOIN
+        if (cmd.equals("join") || cmd.equals("j")) {
+            handleJoinCommand(parts, postLogin, authToken);
+            return;
+        }
+
+        // watch
+        if (cmd.equals("watch") || cmd.equals("w")) {
+            handleWatchCommand(parts, postLogin, authToken);
+            return;
+        }
+
+        handlePostLoginEval(line, postLogin);
+    }
+
+    private void handleJoinCommand(String[] parts, PostLoginClient postLogin, String authToken){
+        if (parts.length != 3) {
+            System.out.println("Usage: join <gameNumber> <white|black>");
+            return;
+        }
+
+        int gameNum = parseInt(parts[1]);
+        String color = parts[2].toLowerCase();
+
+        List<GameData> games = postLogin.getLastListedGames();
+        if (gameNum < 1 || gameNum > games.size()) {
+            System.out.println("Invalid game number. Use 'list' to see games.");
+            return;
+        }
+
+        GameData game = games.get(gameNum - 1);
+
+        boolean joined;
+        try {
+            joined = postLogin.getFacade().joinGame(color, game.gameID());
+        } catch (Exception ex) {
+            System.out.println("Join failed: " + ex.getMessage());
+            return;
+        }
+        if (!joined) {
+            return;
+        }
+
+        ChessGame.TeamColor perspective;
+        if ("white".equalsIgnoreCase(color)) {
+            perspective = ChessGame.TeamColor.WHITE;
+        } else {
+            perspective = ChessGame.TeamColor.BLACK;
+        }
+
+        try {
+            inGame = new InGameClient(serverUrl, authToken, game.gameID(), perspective);
+            state = UiState.IN_GAME;
+            System.out.println("Joined game as " + color + ".");
+            System.out.println(inGame.welcome());
+            System.out.println(inGame.help());
+        } catch (Exception e) {
+            System.out.println("Failed to enter game: " + e.getMessage());
+        }
+    }
+
+
 }
